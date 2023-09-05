098e3ddc54f5f4a17a155f9bd52b2a2c226759eb
vbalko <vbalko@gmail.com>
2023-09-05 23:24:37 +0000

    new tests

--- src/versionManager.js
+++ src/versionManager.js
@@ -24,6 +24,8 @@ const incrementMajorVersion = (currentVersion) => {
     // Increment major version
     let versionArray = currentVersion.split(".");
     versionArray[0] = parseInt(versionArray[0]) + 1;
+    versionArray[1] = 0;
+    versionArray[2] = 0;
     return versionArray.join(".");
 };
 
@@ -32,6 +34,7 @@ const incrementMinorVersion = (currentVersion) => {
     // Increment minor version
     let versionArray = currentVersion.split(".");
     versionArray[1] = parseInt(versionArray[1]) + 1;
+    versionArray[2] = 0;
     return versionArray.join(".");
 };
 
--- test/versionManager.test.js
+++ test/versionManager.test.js
@@ -24,19 +24,42 @@ describe('versionManager', () => {
     });
   });
 
-  describe('#incrementMajorVersion()', () => {
+  describe('#incrementVersion()', () => {
     it('should increment the major version number', () => {
       const currentVersion = '1.2.3';
       const expectedVersion = '2.0.0';
-      const newVersion = versionManager.incrementMajorVersion(currentVersion);
+      const newVersion = versionManager.incrementVersion(currentVersion, "major");
       assert.strictEqual(newVersion, expectedVersion);
     });
 
-    it('should reset the minor and patch version numbers to 0', () => {
+    it('should increment the minor version number', () => {
       const currentVersion = '1.2.3';
-      const expectedVersion = '2.0.0';
-      const newVersion = versionManager.incrementMajorVersion(currentVersion);
+      const expectedVersion = '1.3.0';
+      const newVersion = versionManager.incrementVersion(currentVersion, "minor");
       assert.strictEqual(newVersion, expectedVersion);
     });
-  });
+
+    it('should increment the patch version number', () => {
+      const currentVersion = '1.2.3';
+      const expectedVersion = '1.2.4';
+      const newVersion = versionManager.incrementVersion(currentVersion, "patch");
+      assert.strictEqual(newVersion, expectedVersion);
+    });
+  }
+  );
+
+  describe('#readAndWriteManifest()', () => {
+    it('should read the manifest data from the manifest file and write the new version to the manifest data', () => {
+      const manifestFile = './test/webapp/manifest.json';
+      const manifestData = versionManager.readManifest(manifestFile);
+      //assert that file exists
+      assert.ok(manifestData);
+    });
+  } 
+  );
+
+
+
+
+
 });
